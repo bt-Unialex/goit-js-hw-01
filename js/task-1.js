6f1edfb0b8f8baf64544c6f6202a92415c160dfb
bt-Unialex <Unialex@ukr.net>
2024-12-08 19:36:22 +0200

    removed square brackets

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${[quantity * pricePerDroid]} credits!`;
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 console.log('\n----------------------Task 1----------------------\n');
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
